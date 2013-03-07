60e0ec5ae603c046f4d19c3c84e7dc8f9899888c
Douglas C. Macedo <dodop3000@gmail.com>
2013-03-07 14:08:30 -0300

    exps do livro 'C: The Programming Language'

---
+++ avulsos/conversor.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define INICIO 0
+#define FIM 300
+#define INCR 20
+
+main()
+{
+    int fahr;
+
+    for(fahr = INICIO; fahr <= FIM; fahr = fahr + INCR)
+        printf("%4d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
+
+}
