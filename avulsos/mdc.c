a3acf8a511aa6336a0373b48d93c07a931ab4c55
Douglas Costa <dodop3000@gmail.com>
2013-09-14 20:23:00 -0300

    Programa simples parra calculo do mdc de dois números

---
+++ avulsos/mdc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void mdc(int u, int v);
+
+int main()
+{
+    int first, second;
+
+    printf("Eu quero saber o Máximo Divisor Comum de: ");
+    scanf("%i %i", &first, &second);
+
+    mdc(first, second);
+}
+
+void mdc(int u, int v)
+{
+    int temp;
+    printf("O mdc de %i e %i é ", u, v);
+
+    while (v != 0) {
+        temp = u % v;
+        u = v;
+        v = temp;
+    }
+    printf("%i\n", u);
+}
